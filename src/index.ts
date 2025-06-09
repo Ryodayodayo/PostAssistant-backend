0a6e1f0f881b3cb46f1a42ec337316500b68b1d9
Ryodayodayo <mitosara10@gmail.com>
2025-06-10 02:50:52 +0900

    テスト用配列データを使って認証できるようになった

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ const app = new Hono()
 
 app.use('*', cors())
 
+// テスト用ユーザーデータ
+const users = [
+  { id: 'ryo', password: 'dayo'},
+  { id: 'user1', password: '1'},
+  { id: 'user2', password: '2'},
+  { id: 'user3', password: '3'}
+]
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
@@ -12,11 +20,21 @@ app.get('/', (c) => {
 app.post('/api/login', async (c) => {
   const { id, password } = await c.req.json()
 
-  if (id === 'a' && password === 'b') {
-    return c.json({ success: true, message: 'ログイン成功' })
+  const user = users.find(element => element.id === id && element.password === password)
+
+  if (user) {
+    return c.json({ 
+
+      success: true, 
+      message: 'ログイン成功',
+      userId: user.id, 
+
+    }, 200)
+
   } else {
     return c.json({ success: false, message: 'IDまたはパスワードが間違っています' }, 401)
-  }
+  } 
+
 })
 
 export default app
